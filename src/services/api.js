d83645b29d6dad28399c36df1b0b5eaece3aeec6
Suraj-911 <sgolambade09@gmail.com>
2026-07-07 15:19:22 +0530

    Fix production API URL

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://employee-attendance-backend-00m5.onrender.com"
+  baseURL: "https://employee-attendance-backend-00m5.onrender.com",
 });
 
 // Automatically attach JWT token
